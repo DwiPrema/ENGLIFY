fa259bd2915f53318609278329ac7268d5184aeb
DwiPrema <madedwiprema08@gmail.com>
2025-12-11 15:15:48 +0800

    fix merge conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:englify_project/pages/page_adjective.dart';
 import 'package:englify_project/pages/page_alphabet.dart';
 import 'package:englify_project/pages/page_animal.dart';
@@ -36,42 +35,3 @@ class Englify extends StatelessWidget {
     );
   }
 }
-=======
-import 'package:englify_project/pages/page_adjective.dart';
-import 'package:englify_project/pages/page_alphabet.dart';
-import 'package:englify_project/pages/page_animal.dart';
-import 'package:englify_project/pages/page_fruit.dart';
-import 'package:englify_project/pages/page_greeting.dart';
-import 'package:englify_project/pages/page_noun.dart';
-import 'package:englify_project/pages/page_number.dart';
-import 'package:englify_project/pages/page_verb.dart';
-import 'package:englify_project/pages/splash_page.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const Englify());
-}
-
-class Englify extends StatelessWidget {
-  const Englify({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/alphabetPage': (context) => const AlphabetPage(),
-        '/numberPage': (context) => const NumberPage(),
-        '/fruitPage': (context) => const FruitPage(),
-        '/animalPage': (context) => const AnimalPage(),
-        '/greetingPage': (context) => const GreetingPage(),
-        '/nounPage': (context) => const NounPage(),
-        '/adjectivePage': (context) => const AdjectivePage(),
-        '/verbPage': (context) => const VerbPage(),
-      },
-      home: const Splash(),
-    );
-  }
-}
->>>>>>> 7d3a9635dfac6d07abaae3400e213fe01f11448a
--- lib/widgets/widget_button.dart
+++ lib/widgets/widget_button.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 
 Widget btn(double width, double height, Color bgcolor, Color foreColor,
@@ -26,32 +25,3 @@ Widget btn(double width, double height, Color bgcolor, Color foreColor,
     ),
   );
 }
-=======
-import 'package:flutter/material.dart';
-
-Widget btn(double width, double height, Color bgcolor, Color foreColor,
-    String text, VoidCallback onPres) {
-  return Container(
-    width: width,
-    height: height,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(50),
-    ),
-    child: ElevatedButton(
-      onPressed: onPres,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: bgcolor,
-        foregroundColor: foreColor,
-      ),
-      child: Text(
-        text,
-        style: const TextStyle(
-          fontFamily: "JosefinSans",
-          fontWeight: FontWeight.w700,
-          fontSize: 24,
-        ),
-      ),
-    ),
-  );
-}
->>>>>>> 7d3a9635dfac6d07abaae3400e213fe01f11448a
