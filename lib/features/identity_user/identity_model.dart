4713baa8507ade82b6ae0b5c5b797e2ad5d58c59
DwiPrema <madedwiprema08@gmail.com>
2025-12-27 14:31:20 +0800

    rename file

---
+++ lib/features/identity_user/identity_model.dart
@@ -0,0 +1,8 @@
+class ModelIdentity {
+  final String name;
+  final int age;
+  final DateTime birthday;
+
+  ModelIdentity(
+      {required this.name, required this.age, required this.birthday});
+}
