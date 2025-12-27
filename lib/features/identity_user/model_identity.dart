c16f54c123cccfe442e9c6d3207f24156b003872
DwiPrema <madedwiprema08@gmail.com>
2025-12-27 14:37:13 +0800

    delete unused file

--- lib/features/identity_user/model_identity.dart
+++
@@ -1,8 +0,0 @@
-class ModelIdentity {
-  final String name;
-  final int age;
-  final DateTime birthday;
-
-  ModelIdentity(
-      {required this.name, required this.age, required this.birthday});
-}
