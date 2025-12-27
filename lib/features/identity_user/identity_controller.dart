655248e55a6826d212188d566f9d710b5ccb051a
DwiPrema <madedwiprema08@gmail.com>
2025-12-27 14:31:44 +0800

    rename file

---
+++ lib/features/identity_user/identity_controller.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:project_englify/features/identity_user/identity_model.dart';
+
+class ControllerIdentity {
+  final nameController = TextEditingController();
+  final ageController = TextEditingController();
+  final dateController = TextEditingController();
+
+  DateTime? pickedDate;
+
+  void dispose() {
+    nameController.dispose();
+    ageController.dispose();
+    dateController.dispose();
+  }
+
+  bool isDataValid() {
+    return nameController.text.trim().isNotEmpty &&
+        ageController.text.isNotEmpty &&
+        pickedDate != null;
+  }
+
+  void setDate(DateTime selectedDate) {
+    pickedDate = selectedDate;
+    dateController.text =
+        "${selectedDate.day} - ${selectedDate.month} - ${selectedDate.year}";
+  }
+
+  ModelIdentity createIdentity() {
+    return ModelIdentity(
+        name: nameController.text.trim(),
+        age: int.parse(ageController.text),
+        birthday: pickedDate!);
+  }
+}
