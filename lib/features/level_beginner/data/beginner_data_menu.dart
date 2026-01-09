400e52b1a7cfc5756b6980ab9b04770b4d13e691
DwiPrema <madedwiprema08@gmail.com>
2026-01-09 19:21:29 +0800

    add data menu on level beginner

---
+++ lib/features/level_beginner/data/beginner_data_menu.dart
@@ -0,0 +1,44 @@
+import 'package:project_englify/core/routes/routes.dart';
+
+const beginnerMenuItems = [
+  {
+    'title': 'Alphabet',
+    'subtitle': '(Abjad)',
+    'route': AppRoutes.alphabetPageRoute,
+  },
+  {
+    'title': 'Number',
+    'subtitle': '(Angka)',
+    'route': AppRoutes.numberPageRoute,
+  },
+  {
+    'title': 'Fruit',
+    'subtitle': '(Buah)',
+    'route': AppRoutes.fruitPageRoute,
+  },
+  {
+    'title': 'Animal',
+    'subtitle': '(Hewan)',
+    'route': AppRoutes.animalPageRoute,
+  },
+  {
+    'title': 'Greeting',
+    'subtitle': '(Salam)',
+    'route': AppRoutes.greetingPageRoute,
+  },
+  {
+    'title': 'Noun',
+    'subtitle': '(Kata Benda)',
+    'route': AppRoutes.nounPageRoute,
+  },
+  {
+    'title': 'Adjective',
+    'subtitle': '(Kata Sifat)',
+    'route': AppRoutes.adjectivePageRoute,
+  },
+  {
+    'title': 'Verb',
+    'subtitle': '(Kata Kerja)',
+    'route': AppRoutes.verbPageRoute,
+  },
+];
